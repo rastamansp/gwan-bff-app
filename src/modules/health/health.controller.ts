7938a5279cc0025ec22397ff619c693730fdcef6
Pedro Almeida <pedro.hp.almeida@gmail.com>
2025-05-11 00:02:40 -0300

    correcao health

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Head } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @ApiTags("health")
 @Controller("health")
 export class HealthController {
   @Get()
+  @Head()
   @ApiOperation({ summary: "Health check endpoint" })
   @ApiResponse({ status: 200, description: "Service is healthy" })
   check() {
